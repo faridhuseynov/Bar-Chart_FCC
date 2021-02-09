36a894dd0fc20a2591f89fd68c24da02c2122331
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-09 23:40:29 +0400

    Changed the data for years to scaleTime()

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
 const height = 700;
-const width = 900;
-const padding = 60;
+const width = 700;
+const padding = 40;
 
 var fetched_data = [];
 $(document).ready(() => {
@@ -12,7 +12,7 @@ $(document).ready(() => {
     .then((data) => {
       fetched_data = data;
 
-      console.log(fetched_data);
+      // console.log(fetched_data);
 
       $("#title").text(fetched_data.source_name);
 
@@ -25,33 +25,29 @@ $(document).ready(() => {
       const dataset = fetched_data.data;
       const from = fetched_data.from_date;
       const to = fetched_data.to_date;
-      const length = fetched_data.data.length;
+      const bar_width = fetched_data.data.length/width;
 
-      const min_year = parseInt(from.split("-")[0]);
-      const max_year = parseInt(to.split("-")[0]);
+      // const min_year = parseInt(from.split("-")[0]);
+      // const max_year = parseInt(to.split("-")[0]);
 
-      // const min_year_msecs = Date.parse(from);
-      // const max_year_msecs = Date.parse(to);
-
-      // console.log(min_year_msecs, max_year_msecs);
-      // const min_year = fetched_data.from_date;
-      // const max_year = fetched_data.to_date;
+      const min_year = new Date(from);
+      const max_year = new Date(to);
 
       const min_gdp = parseInt(d3.min(dataset, (d) => d[1]));
-      const max_gdp = parseInt(d3.max(dataset, (d) => d[1])+1);
+      const max_gdp = parseInt(d3.max(dataset, (d) => d[1]));
 
-      console.log(min_year, max_year);
-      console.log(min_gdp, max_gdp);
+      // console.log(min_year, max_year);
+      // console.log(min_gdp, max_gdp);
 
       const xScale = d3
-        .scaleLinear()
+        .scaleTime()
         .domain([min_year, max_year])
         .range([padding, width - padding]);
 
       const yScale = d3
         .scaleLinear()
         .domain([min_gdp, max_gdp])
-        .range([height - padding, padding]);
+        .range([height-padding, padding]);
 
       const xAxis = d3.axisBottom(xScale);
       const yAxis = d3.axisLeft(yScale);
@@ -72,7 +68,12 @@ $(document).ready(() => {
       // var month=0;
       // var day=0;
       
-      console.log(max_gdp);
+      // console.log(max_gdp);
+      var years = dataset.map((year)=>{
+        return new Date(year);
+      })
+
+      var yearScaledData = 
       svg
         .selectAll("rect")
         .data(dataset)
@@ -83,9 +84,15 @@ $(document).ready(() => {
           // return ((d[0].split("-")[0])+i*3);
           return padding+i*3;
         })
-        .attr("y", (d) => yScale(max_gdp-(d[1])))
-        .attr("height", ((d) => yScale([d[1]])))
+        .attr("y", (d) =>{
+          console.log(d[1]);
+          console.log(yScale(d[1]));
+          console.log(height-yScale((d[1])));
+          return height-yScale((d[1]))          
+        })
+        .attr("height", ((d) => ([d[1]])))
         .attr("width", 2);
-        console.log(dataset);
+
+        // console.log(dataset);
     });
 });
